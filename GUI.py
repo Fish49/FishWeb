0b31564b7693912414ee4703c475b8a23b14ff2b
Joseph Calabro <beebfish49@gmail.com>
2024-11-11 23:28:28 -0700

    adds icon, starts using tkinter, and starts regretting life choices.

--- GUI.py
+++ GUI.py
@@ -2,4 +2,21 @@
 gui stuff for FishWeb
 -PaiShoFish49
 '''
-import pywin32
+import tkinter as tk
+import tkinter.ttk as ttk
+from ttkthemes import ThemedTk
+
+root = ThemedTk("FishWeb Browser", theme="black")
+# root.iconbitmap('fishweb.ico')
+root.geometry('200x200')
+
+pageArea = ttk.Frame(root)
+pageArea.pack()
+
+tabBrowser = ttk.Frame(root)
+tabBrowser.pack()
+
+button = ttk.Button(tabBrowser, text='joemama')
+button.pack()
+
+root.mainloop()
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FishWeb
 Web development is hard, so Im gonna try and make my own web (just for fun).
+
+## The Plan:
+my plan is to create a version of the web thats more simple, intuitive, and powerful. if used Svelte before and liked the reactivity, so i plan to have reactivity built right in, just like svelte.
