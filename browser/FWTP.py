40cc805e0fcb20f6f3b2839cf7bd20b74af48c58
Joseph Calabro <beebfish49@gmail.com>
2024-11-18 23:56:50 -0700

    starts to actually understand sockets.

--- browser/FWTP.py
+++ browser/FWTP.py
@@ -4,27 +4,5 @@ fish web transfer protocol
 
 import socket
 
-class Listener():
-    def __init__(self, isFromIp: True, client: str | socket.socket, onRecieve: function):
-        if isFromIp:
-            ipPort = client.split(":")
-            self.ip = ipPort[0]
-            if len(ipPort) > 1:
-                self.port = ipPort[1]
-        
-        else:
-            self.ip = socket._Address
-
-        self.onRecieve = onRecieve
-
-    
-
-# def sendToIp(IP, data):
-
-
-# def getIpFromURL(URL, DNS):
-#     request
-
-# def getPage()
-
-socket.
+def setupClientConnection():
+    return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
