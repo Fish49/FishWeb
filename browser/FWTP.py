6376ecf671280c0f38122277ab8d5a70c9993469
Joseph Calabro <beebfish49@gmail.com>
2024-11-19 23:58:59 -0700

    keeps working on transfer protocol

--- browser/FWTP.py
+++ browser/FWTP.py
@@ -3,6 +3,20 @@ fish web transfer protocol
 '''
 
 import socket
+import json
 
-def setupClientConnection():
-    return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def setupClientConnection(IP = '0.0.0.0', port = 5154, useUDP = False):
+    if useUDP:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    else:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.bind((IP, port))
+    return s
+
+def sendProgData(data, socket: socket.socket):
+    message = {
+        "tranfer": "progData",
+        "data": data
+    }
+    socket.send(json.dumps(message))
